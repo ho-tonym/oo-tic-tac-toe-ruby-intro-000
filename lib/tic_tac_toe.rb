e59ab28765657b0394b88db5a3892ebaafb1046d
Tony Ho <ho.tonym@gmail.com>
2018-04-26 14:36:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,16 +18,16 @@ class TicTacToe
     @board = board
   end
 
-  def won?(board)
+  def won?
 
     WIN_COMBINATIONS.each do |win_index|
         win_index_1 = win_index[0]
         win_index_2 = win_index[1]
         win_index_3 = win_index[2]
 
-        board_position_1 = board[win_index_1]
-        board_position_2 = board[win_index_2]
-        board_position_3 = board[win_index_3]
+        board_position_1 = @board[win_index_1]
+        board_position_2 = @board[win_index_2]
+        board_position_3 = @board[win_index_3]
 
         if board_position_1 =="X" && board_position_2 == "X" && board_position_3 =="X"
           return win_index
